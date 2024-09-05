6c0f4cba43ffe80a53ad8c3278a3bb4dd6fcbaef
Gemi <githuba122@gmail.com>
2024-09-05 21:59:45 +0700

    feat: add background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import DisplayWeather from "./components/DisplayWeather"
+import DisplayWeather from "./components/DisplayWeather";
 
 function App() {
   return (
-    <DisplayWeather />    
-  )
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-slate-200">
+      <DisplayWeather />
+    </div>
+  );
 }
 
-export default App
+export default App;
