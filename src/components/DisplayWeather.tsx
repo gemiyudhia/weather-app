03d4a9c10ab006a330739b383545a509bdc0c49e
Gemi <githuba122@gmail.com>
2024-09-05 22:43:47 +0700

    style: add Input component

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -1,3 +1,5 @@
+import Input from "./Input";
+
 const DisplayWeather = () => {
   return (
     <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
@@ -10,7 +12,7 @@ const DisplayWeather = () => {
         from the dropdown
       </p>
 
-      <input type="text" placeholder="Enter a city" className="px-2 py-1 rounded-tl-md rounded-br-md border-white shadow-sm outline-none focus:ring-2 focus:ring-blue-400 mt-10 md:mt-4" />
+      <Input />
     </section>
   );
 };
---
+++ src/components/Input.tsx
@@ -0,0 +1,11 @@
+const Input = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Enter a city"
+      className="px-2 py-1 rounded-tl-md rounded-br-md border-white shadow-sm outline-none focus:ring-2 focus:ring-blue-400 mt-10 md:mt-4"
+    />
+  );
+}
+
+export default Input
