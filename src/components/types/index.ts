51ff99a598314ffe529be787d3bd4430793aaeb3
Gemi <githuba122@gmail.com>
2024-09-09 22:08:51 +0700

    add new type

--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -3,3 +3,45 @@ export type optionType = {
     lat: number,
     lon: number
 }
+
+export type forecastType = {
+  name: string;
+  country: string;
+  sunrise: number;
+  sunset: number;
+  list: [
+    {
+      dt: number;
+      main: {
+        temp: number;
+        feels_like: number;
+        pressure: number;
+        humidity: number;
+        temp_min: number;
+        temp_max: number;
+      };
+      weather: [
+        {
+          description: string;
+        }
+      ];
+    },
+    weather: [
+      {
+        main: string;
+        description: string;
+        icon: string;
+      }
+    ],
+    wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+    },
+    clouds: {
+      all: number;
+    },
+    pop: number,
+    visibility: number
+  ];
+};
